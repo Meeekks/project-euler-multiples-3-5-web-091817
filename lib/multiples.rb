248e8d87ee24b2779a4e43b6d9a50bbc8e1de0e8
github username <github email address>
2017-09-29 15:32:00 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,28 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  array = []
+
+  i = 1
+  while (i < limit)
+    if i % 3 == 0 || i % 5 == 0
+      array.push(i)
+    end
+  i += 1
+  end
+
+  array
+end
+
+
+def sum_multiples(limit)
+  sum = 0
+
+  i = 1
+  while (i < limit)
+    if i % 3 == 0 || i % 5 == 0
+      sum += i
+    end
+  i += 1
+  end
+
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,36 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    array = []
+
+    i = 1
+    while (i < @limit)
+      if i % 3 == 0 || i % 5 == 0
+        array.push(i)
+      end
+    i += 1
+    end
+
+    array
+  end
+
+  def sum_multiples
+    sum = 0
+
+    i = 1
+    while (i < @limit)
+      if i % 3 == 0 || i % 5 == 0
+        sum += i
+      end
+    i += 1
+    end
+
+    sum
+  end
+
+end
